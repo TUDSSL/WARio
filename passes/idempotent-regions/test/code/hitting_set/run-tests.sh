6ca76430df97aae6d6ebb926f3f0ec0d73c750fc
Vito Kortbeek <vito.kortbeek@gmail.com>
2021-06-07 12:58:47 +0200

    [testing] Rework test setup

--- passes/idempotent-regions/test/code/hitting_set/run-tests.sh
+++ passes/idempotent-regions/test/code/hitting_set/run-tests.sh
@@ -1,50 +1,5 @@
 #!/bin/bash
 
-# The .c files contain comment lines starting with '$' that list the expected
-# output of the pass on that code.
-#
-# First the C file is converted to a .ll file using c-to-llvm, and next
-# the pass is applied using pass-apply. The output of that command should
-# contain all the same strings starting wiht '$'
-
-DIR_TO_TEST="$1"
-
-if [ -z $DIR_TO_TEST ]; then
-  TEST_DIRS=("branchless" "branched" "loop")
-else
-  TEST_DIRS=("$DIR_TO_TEST")
-fi
-
-REMOVE_TMP_FILES="true"
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd $SCRIPT_DIR
-
-RUN="$SCRIPT_DIR/run-testcase.sh"
-
-passed_tests=0
-failed_tests=0
-
-for t in ${TEST_DIRS[@]}; do
-  echo "Running tests in directory: $t"
-  pushd $t
-
-  for f in *.c ; do
-    echo ""
-    $RUN "$f" "$REMOVE_TMP_FILES"
-    if [ $? -eq 0 ]; then
-      ((passed_tests=passed_tests+1))
-    else
-      ((failed_tests=failed_tests+1))
-    fi
-  done
-
-  popd
-done
-
-if [ $failed_tests -gt 0 ]; then
-  echo ""
-  echo "SOME TESTS FAILED!"
-fi
-
-exit $failed_tests
+run-tests-dir branchless &&
+run-tests-dir branched &&
+run-tests-dir loop
--- passes/idempotent-regions/test/code/war_paths/run-testcase.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-f="$1"
-
-# Convert the C file to .ll
-c-to-llvm "$f" "$f.ll" > /dev/null 2>&1
-
-# Run the pass on the .ll
-pass-apply -idemp -o "$f.pass.ll" "$f.ll"
