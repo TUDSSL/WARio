2f8ca71b1e424ead164f5fda55c8c5ebf1b37cde
Vito Kortbeek <vito.kortbeek@gmail.com>
2021-06-11 15:53:24 +0200

    [idemp-pass] Add configuration option to add the checkpoint_count variable
    Forgot the header file

--- passes/idempotent-regions/include/Configurations.hpp
+++ passes/idempotent-regions/include/Configurations.hpp
@@ -62,11 +62,7 @@ extern cl::opt<bool> InitExit;
 
 extern cl::opt<bool> Debug;
 
-extern cl::opt<bool> LoopUnrollStep;
-
-extern cl::opt<unsigned int> LoopUnrollCount;
-
-extern cl::opt<bool> LoopScheduleStep;
+extern cl::opt<bool> InsertCheckpointCount;
 
 #ifdef DEBUG_PRINT
 #define dbg() errs()
