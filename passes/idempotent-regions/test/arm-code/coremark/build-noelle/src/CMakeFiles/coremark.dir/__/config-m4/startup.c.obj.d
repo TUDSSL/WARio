1064c1a9c8d1d1aaf0a67be14564906c19b1d892
Vito Kortbeek <vito.kortbeek@gmail.com>
2021-06-29 12:04:29 +0200

    [testing] Remove build dir

--- passes/idempotent-regions/test/arm-code/coremark/build-noelle/src/CMakeFiles/coremark.dir/__/config-m4/startup.c.obj.d
+++
@@ -1,8 +0,0 @@
-src/CMakeFiles/coremark.dir/__/config-m4/startup.c.obj: \
-  /home/vkortbeek/devel/intermittent-compiler/intermittent-compiler-collection/passes/idempotent-regions/test/arm-code/coremark/config-m4/startup.c \
-  /usr/arm-none-eabi/include/stdint.h \
-  /usr/arm-none-eabi/include/machine/_default_types.h \
-  /usr/arm-none-eabi/include/sys/features.h \
-  /usr/arm-none-eabi/include/_newlib_version.h \
-  /usr/arm-none-eabi/include/sys/_intsup.h \
-  /usr/arm-none-eabi/include/sys/_stdint.h
--- passes/idempotent-regions/test/arm-code/coremark/build-noelle/src/CMakeFiles/coremark.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for coremark.
-# This may be replaced when dependencies are built.
