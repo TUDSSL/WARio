4115f273058f788f9de40df7aa9b9ec434bf73b2
Vito Kortbeek <vito.kortbeek@gmail.com>
2021-07-08 11:26:22 +0200

    [project] Add the in-project llvm to the path

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 export PATH="$ROOT_DIR/scripts/:$PATH"
+export PATH="$ROOT_DIR/llvm/llvm-9.0.1/install/bin:$PATH"
+
+export LLVM_ROOT="$ROOT_DIR/llvm/llvm-9.0.1/install/"
