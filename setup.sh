e4c337a61fc52ccb5ff6d79301fc524c6c2c1d19
Vito Kortbeek <vito.kortbeek@gmail.com>
2021-07-21 13:48:16 +0200

    [project] Add DIR info to the setup

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ export PATH="$ROOT_DIR/scripts/:$PATH"
 export PATH="$ROOT_DIR/llvm/llvm-9.0.1/install/bin:$PATH"
 
 export LLVM_ROOT="$ROOT_DIR/llvm/llvm-9.0.1/install/"
+
+export ICLANG_ROOT="$ROOT_DIR"
+export CHECKPOINTLIB="$ROOT_DIR/toolchain-arm/checkpoint"
