06f90b40bceab1d1223af9871b50831e8fc49fc9
Vito Kortbeek <vito.kortbeek@gmail.com>
2021-07-27 18:24:08 +0200

    [project] add noelle install to the setup

--- setup.sh
+++ setup.sh
@@ -4,8 +4,11 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export PATH="$ROOT_DIR/scripts/:$PATH"
 export PATH="$ROOT_DIR/llvm/llvm-9.0.1/install/bin:$PATH"
+export PATH="$ROOT_DIR/noelle/noelle/install/bin:$PATH"
 
 export LLVM_ROOT="$ROOT_DIR/llvm/llvm-9.0.1/install/"
+
+export NOELLE="$ROOT_DIR/noelle/noelle/install"
 
 export ICLANG_ROOT="$ROOT_DIR"
 export CHECKPOINTLIB="$ROOT_DIR/toolchain-arm/checkpoint"
