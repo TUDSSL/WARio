77edd15851f3069f17d824b9891077e073fd897e
Vito Kortbeek <vito.kortbeek@gmail.com>
2021-10-18 11:35:10 +0200

    [checkpoint] add a special checkpoint that does not use the stack
    Instead it uses R2 and R3, which are not normally live during the
    epilog. If they are live, the compiler will know and choose not to
    optimize the epilog. (e.g., when there is a tail-call).

--- toolchain-arm/checkpoint/checkpoint.c
+++ toolchain-arm/checkpoint/checkpoint.c
@@ -149,3 +149,69 @@ __attribute__((alias("__checkpoint"))) void __checkpoint_pop(void);
 __attribute__((alias("__checkpoint"))) void __checkpoint_spill(void);
 #endif /* CHECKPOINT_MARKERS_DISABLE */
 
+
+#ifndef DISABLE_CHECKPOINT_POP_NOSTACK_OPT
+
+/*
+ * A specialized checkpoint call that does not use any stack but uses R2 and R3
+ * which are unused in the epilog of a function.
+ * This checkpoint is used by the compiler if the
+ * '--idemp-disable-interrupt-during-pop' option is provided when possible
+ *
+ * '__cp_tmp' is used as a temporary variable instead of the stack
+ */
+uint32_t __cp_tmp;
+NAKED void __checkpoint_pop_nostack(void) {
+  // We can only use R2 and R3
+  // LR is already in R3 (if LR is live)
+
+  // Save the LR to _cp_tmp
+  LIBCP_ASM("ldr r2, =__cp_tmp");
+  LIBCP_ASM("str r3, [r2]");
+
+  // Load the logical clock location
+  LIBCP_ASM("ldr r3, =__lclock");
+  LIBCP_ASM("ldr r3, [r3]");
+
+  // Load the register checkpoint location (double buffered)
+  LIBCP_ASM("ldr r2, =__registers_checkpoint_ptr");
+  // Get where to store the CP => registers_checkpoint_ptr[lclock]
+  // This will point to either registers_checkpoint_[a|b]
+  LIBCP_ASM("ldr r2, [r2, r3, lsl #2]");
+
+  // Restore R3/LR
+  LIBCP_ASM("ldr r3, =__cp_tmp");
+  LIBCP_ASM("ldr r3, [r3]");
+
+  // Store r3-r12 into the checkpoint
+  LIBCP_ASM("stmia r2!, {r3-r12}");   // store r3-r12
+
+  LIBCP_ASM("mov r4, r3");            // Load the LR stored in R3 before the call NOTE: This is set before the checkpoint if LR is live
+
+  LIBCP_ASM("mov r3, sp");            // load sp into r3
+
+  LIBCP_ASM("mrs r5, xpsr");          // load psr into r5
+
+  LIBCP_ASM("mov r6, r0");     // Load r0-r2 into r6-r8
+  LIBCP_ASM("mov r7, r1");     // Load r0-r2 into r6-r8
+  LIBCP_ASM("mov r8, r2");     // Load r0-r2 into r6-r8 NOT NEEDED
+
+  LIBCP_ASM("stmia r2!, {r3-r8}");    // store sp, lr, psr, r0, r1, r2
+
+  LIBCP_ASM("str lr, [r2]");          // store the return LR at the end of the checkpoint
+  LIBCP_ASM("mov lr, r4");            // move the LR before the checkpoint call back to lr
+
+  // Logical clock management
+  LIBCP_ASM("ldr r3, =__lclock");
+  LIBCP_ASM("ldr r4, [r3]");
+
+  LIBCP_ASM("eor r4, r4, #1");        // XOR the logical clock (1=>0, 0=>1)
+  LIBCP_ASM("str r4, [r3]");          // Store the logical clock CHECKPOINT
+
+  // Restore the registers before return (never executed during restore)
+  LIBCP_ASM("sub r2, #64");           // Point to r3-r8
+  LIBCP_ASM("ldmia r2, {r3-r8}");     // restore r3-r8
+  LIBCP_ASM("add r2, #52");           // Point to r0-r2
+  LIBCP_ASM("ldmia r2, {r0-r2, pc}"); // restore r0-r2 and load the return LR into PC
+}
+#endif
