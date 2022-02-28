432dffa0264cc51c827e28878a8b1be85e2fe34b
Vito Kortbeek <vito.kortbeek@gmail.com>
2022-02-28 19:57:13 +0100

    Update benchmark build scripts

---
+++ benchmarks/build-run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Build **and run** all the benchmarks
+# Compilation (generating the PDG using Noelle) and emulation (using ICEmu)
+# Are both CPU and Memory intensive. For 16GB of system memory we recoment not
+# using more than 1 thread.
+# This limitation is mainly due to the larger 'picojpeg' benchmark.
+make -j 1
